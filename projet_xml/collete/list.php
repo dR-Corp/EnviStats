4bef98b24dc9ac5de6a9fa46c2b391871958249d
DJERI Rayid <rayidjeri@gmail.com>
2023-02-20 22:28:31 +0100

    Fourth commit, stable version before adding graphics

--- projet_xml/collete/list.php
+++
@@ -1,4 +0,0 @@
-<?php 
-    include("../fonctions.php");
-    $list_categories = get_all_collectes();
-?>
--- projet_xml/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-    include('fonctions.php');
-?>
-
-<!-- cette page contiendra le dashboard qui ici est l apge d'accueil et sera le point d'entré de l'appplication, le home controller en gros -->
