077d50913f1825273db3d80c0f580390f118112e
DJERI Rayid <rayidjeri@gmail.com>
2023-02-20 23:19:15 +0100

    Fisrt commit

--- Views/dashboard.php
+++ Views/dashboard.php
@@ -71,6 +71,52 @@
         <!-- /.row -->
 
         <div class="row">
+
+            <div class="col-md-12">
+            <canvas id="myChart"></canvas>
+            <script>
+                var ctx = document.getElementById('myChart').getContext('2d');
+                var myChart = new Chart(ctx, {
+                    type: 'bar',
+                    data: {
+                        labels: ['2019', '2020', '2021'],
+                        datasets: [
+                            {
+                                label: 'Banikanni',
+                                data: [12, 13, 15],
+                                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                                borderColor: 'rgba(255, 99, 132, 1)',
+                                borderWidth: 1
+                            },
+                            {
+                                label: 'Zongo',
+                                data: [40, 20, 19],
+                                backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                                borderColor: 'rgba(54, 162, 235, 1)',
+                                borderWidth: 1
+                            },
+                            {
+                                label: 'Wore',
+                                data: [10, 14, 9],
+                                backgroundColor: 'rgba(255, 206, 86, 0.2)',
+                                borderColor: 'rgba(255, 206, 86, 1)',
+                                borderWidth: 1
+                            }
+                        ]
+                    },
+                    options: {
+                        scales: {
+                            yAxes: [{
+                                ticks: {
+                                    beginAtZero: true
+                                }
+                            }]
+                        }
+                    }
+                });
+            </script>
+            </div>
+
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">
